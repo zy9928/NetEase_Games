a55caa3d5fd7414c8134df54fe5177451a6691d9
YuanZheng <992832548@qq.com>
2019-09-19 21:15:48 +0800

    sry cpz

--- app/static/scripts/wy_play_test.js
+++ app/static/scripts/wy_play_test.js
@@ -1,8 +1,6 @@
 export function test(){
     var cpz_test_li = $('.test-case li');
     var cpz_test_index = 0;
-    console.log($('.test-case li'));
-    console.log(cpz_test_li[cpz_test_index]);
     cpz_test_li[cpz_test_index].className='case1 active';
     cpz_test_li.mouseenter(function(){
         $(this).stop().animate({width:562});
